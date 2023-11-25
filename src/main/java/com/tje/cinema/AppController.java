a443a68ffdb9a3e3292fc4e90e4c95467ac02630
Karol-2 <97386279+Karol-2@users.noreply.github.com>
2023-11-25 18:49:00 +0100

    Dodanie kilku sciezek

--- src/main/java/com/tje/cinema/AppController.java
+++ src/main/java/com/tje/cinema/AppController.java
@@ -3,6 +3,7 @@ package com.tje.cinema;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.text.ParseException;
 
@@ -11,6 +12,31 @@ public class AppController {
 
     @GetMapping("/")
     public String home(Model model) throws ParseException {
-        return "home";
+        return "homePage";
+    }
+
+    @GetMapping("/login")
+    public String login(Model model) throws ParseException {
+        return "loginPage";
+    }
+
+    @GetMapping("/register")
+    public String register(Model model) throws ParseException {
+        return "registerPage";
+    }
+
+    @GetMapping("/movies")
+    public String movies(Model model) throws ParseException {
+        return "moviesPage";
+    }
+
+    @GetMapping("/movies/details/{movieId}")
+    public String movieDetails(@PathVariable Long movieId, Model model) throws ParseException {
+
+//        Movie movie = movieService.getMovieById(movieId);
+//
+//        model.addAttribute("movie", movie);
+//
+        return "movieDetailsPage";
     }
 }
