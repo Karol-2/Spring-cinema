ead1f9de236b910f67ccdfc6aca23b7d3138db0d
Karol-2 <97386279+Karol-2@users.noreply.github.com>
2023-12-01 12:01:58 +0100

    Dodanie walidacji

--- src/main/java/com/tje/cinema/controllers/UserController.java
+++ src/main/java/com/tje/cinema/controllers/UserController.java
@@ -4,6 +4,7 @@ import com.tje.cinema.domain.User;
 import com.tje.cinema.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -17,11 +18,19 @@ public class UserController {
     public String registerUser(@RequestParam("email") String email,
                                @RequestParam("name") String name,
                                @RequestParam("password") String password,
-                               @RequestParam("passwordConf") String passwordConf) {
+                               @RequestParam("passwordConf") String passwordConf,
+                               Model model){
 
         if (!passwordConf.equals(password)){
-            return "redirect:/registerPage";
+            model.addAttribute("error", "Passwords do not match");
+            return "registerPage";
         }
+
+        try {
+            User user = userService.getUserByEmail(email);
+            model.addAttribute("error", "User with this email already exists!");
+            return "registerPage";
+        } catch (RuntimeException ignored){}
 
         User newUser = new User(email, name, password);
         userService.saveUser(newUser);
